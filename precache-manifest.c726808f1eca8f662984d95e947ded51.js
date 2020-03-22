8068bf4645dd35572c5c7633e3214db3e8e5ba34
Imanghvs <60855317+Imanghvs@users.noreply.github.com>
2020-03-22 18:51:44 +0430

    Updates

---
+++ precache-manifest.c726808f1eca8f662984d95e947ded51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c3dd43509c0d642eb7048f7bb3a777",
+    "url": "/Nowruz/index.html"
+  },
+  {
+    "revision": "0b38dc3e83475f5043b3",
+    "url": "/Nowruz/static/css/main.b178a2d6.chunk.css"
+  },
+  {
+    "revision": "4a63ce37d5a2d287a004",
+    "url": "/Nowruz/static/js/2.f5f68336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nowruz/static/js/2.f5f68336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b38dc3e83475f5043b3",
+    "url": "/Nowruz/static/js/main.863dfdbd.chunk.js"
+  },
+  {
+    "revision": "3b9796a3bf5d110a8085",
+    "url": "/Nowruz/static/js/runtime-main.733e3177.js"
+  }
+]);
